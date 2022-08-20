4e59a201dba52b12ceeb13208ba0009da34349d8
randomplayer2002 <76877728+randomplayer2002@users.noreply.github.com>
2022-08-20 18:26:03 +0530

    Done

--- DijkstrasAlgorithm.java
+++ DijkstrasAlgorithm.java
@@ -1,77 +1,79 @@
 package Daa;
-import java.util.*;
+
+import java.util.Scanner;
 public class DijkstrasClass {
-	final static int MAX = 20;
-	final static int infinity = 9999;
-	static int n;				// No. of vertices of G
-	static int a[][];				// Cost matrix
+	static int a[][];
+	static int n;
 	
-static Scanner scan = new Scanner(System.in);
-
-	public static void main(String[] args) 
-	{
-		ReadMatrix();
-		int s = 0; 			// starting vertex
-		System.out.println("Enter starting vertex: ");
-		s = scan.nextInt();
-		Dijkstras(s);  			// find shortest path
-	}
-
-	static void ReadMatrix() 
-	{
-		a = new int[MAX][MAX];
-		System.out.println("Enter the number of vertices:");
-		n = scan.nextInt();
-		System.out.println("Enter the cost adjacency matrix:");
-		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= n; j++)
-				a[i][j] = scan.nextInt();
-	}
-
-	static void Dijkstras(int s) 
-	{
-		int S[] = new int[MAX];
-		int d[] = new int[MAX];
-		int u, v;
-		int i;
-		for (i = 1; i <= n; i++) 
-		{
-			S[i] = 0;
-			d[i] = a[s][i];
-		}
-		S[s] = 1;
-		d[s] = 1;
-		i = 2;
-		
-		while (i <= n) 
-		{
-			u = Extract_Min(S, d);
-			S[u] = 1;
-			i++;
-			for (v = 1; v <= n; v++) 
-			{
-				if (((d[u] + a[u][v] < d[v]) && (S[v] == 0)))
-					d[v] = d[u] + a[u][v];
-			}
-		}
-		for (i = 1; i <= n; i++)
-			if (i != s)
-				System.out.println(i + ":" + d[i]);
-	}
-
-	static int Extract_Min(int S[], int d[]) 
-	{
-		int i, j = 1, min;
-		min = infinity;
-		for (i = 1; i <= n; i++) 
-		{
-			if ((d[i] < min) && (S[i] == 0)) 
-			{
-				min = d[i];
-				j = i;
-			}
-		}
-		return j;
-	}
-
+	 public static void main(String args[])
+	 {
+		 Scanner input = new Scanner(System.in);
+		 System.out.println("Enter the number of vertices: ");
+		 n = input.nextInt();
+		 System.out.println("Enter the cost adjacency matrix: ");
+		 a = new int[n][n];
+		 
+		 for (int i=0;i<n;i++)
+		 {
+			 for (int j=0;j<n;j++)
+			 {
+				 a[i][j] = input.nextInt();
+			 }
+		 }
+		 
+		 System.out.println("\nEnter the source vertex: ");
+		 int s=input.nextInt();
+		 
+		 Dijkstra(s);
+		 input.close();
+	 }
+	
+	 public static void Dijkstra(int s)
+	 {
+		 int visited[] = new int[n];
+		 int d[] = new int[n];
+		 int i,u,v;
+		 
+		 for(i=0;i<n;i++)
+		 {
+			 visited[i]=0;
+			 d[i] = a[s][i];
+		 }
+		 
+		 visited[s]=1;
+		 d[s]=0;
+		 i=1;
+		 
+		 while(i<=n-1)
+		 {
+			 u = Extract_Min(visited,d);
+			 visited[u]=1;
+			 i++;
+			 
+			 for(v=0;v<n;v++)
+			 {
+				 if((d[u]+a[u][v]<d[v]) && visited[v]==0)
+					 d[v]= d[u]+a[u][v];
+			 }
+		 }
+		 
+		 for(i=0;i<n;i++)
+		 {
+			if(i!=s)
+				System.out.println(s+"->"+i+":"+d[i]);
+		 }
+	 }
+	
+	 public static int Extract_Min(int visited[],int d[])
+	 {
+		 int i,j=0,min=999;
+		 
+		 for(i=0; i<n; i++)
+			 if(d[i]<min && visited[i]==0)
+			 {
+				 min = d[i];
+				 j=i;
+			 }
+		 return j;
+	 }
 }
