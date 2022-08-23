48edb783e4300b0450bb4e0b29c3a53cdf465f6c
randomplayer2002 <76877728+randomplayer2002@users.noreply.github.com>
2022-08-23 11:05:15 +0530

    Done

--- FloydsAlgorithm(SP).java
+++ FloydsAlgorithm(SP).java
@@ -1,53 +1,45 @@
 package Daa;
 import java.util.Scanner;
-public class FloydsClass {
-	static final int MAX = 20; 		// max. size of cost matrix
-	static int a[][];				// cost matrix
-	static int n; 				// actual matrix size
-
-	public static void main(String args[]) 
-	{
-		a = new int[MAX][MAX];
-		ReadMatrix();
-		Floyds();			// find all pairs shortest path
-		PrintMatrix();
-	}
-
-	static void ReadMatrix() 
-	{
-		System.out.println("Enter the number of vertices\n");
-		Scanner scanner = new Scanner(System.in);
-		n = scanner.nextInt();
-		System.out.println("Enter the Cost Matrix (999 for infinity) \n");
-		for (int i = 1; i <= n; i++) 
-		{
-			for (int j = 1; j <= n; j++) 
-			{
-				a[i][j] = scanner.nextInt();
-			}
-		}
-		scanner.close();
-	}
-	
-	static void Floyds() 
-	{
-		for (int k = 1; k <= n; k++)
-		{
-			for (int i = 1; i <= n; i++)
-				for (int j = 1; j <= n; j++)
-					if ((a[i][k] + a[k][j]) < a[i][j])
-						a[i][j] = a[i][k] + a[k][j];
-		}
-	}
-	
-	static void PrintMatrix() 
-	{
-		System.out.println("The All Pair Shortest Path Matrix is:\n");
-		for(int i=1; i<=n; i++)
-		{
-			for(int j=1; j<=n; j++)
-				System.out.print(a[i][j] + "\t");
-			System.out.println("\n");
-		}
-	}
+public class Floyds
+{
+static int a[][];
+static int n;
+public static void main(String args[])
+{
+System.out.println("Enter the number of vertices\n");
+Scanner scanner = new Scanner(System.in);
+n = scanner.nextInt();
+a = new int[n][n];
+System.out.println("Enter the Cost Matrix (999 for infinity) \n");
+for (int i = 0; i < n; i++)
+{
+for (int j = 0; j < n; j++)
+{
+a[i][j] = scanner.nextInt();
+}
+}
+getPath();
+PrintMatrix();
+scanner.close();
+}
+public static void getPath()
+{
+for (int k = 0; k < n; k++)
+{
+for (int i = 0; i < n; i++)
+for (int j = 0; j < n; j++)
+if ((a[i][k] + a[k][j]) < a[i][j])
+a[i][j] = a[i][k] + a[k][j];
+}
+}
+public static void PrintMatrix()
+{
+System.out.println("The All Pair Shortest Path Matrix is:\n");
+for(int i=0; i<n; i++)
+{
+for(int j=0; j<n; j++)
+System.out.print(a[i][j] +" ");
+System.out.println();
+}
+}
 }
