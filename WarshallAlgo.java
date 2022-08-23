86a0da4384c3727f9405bebe35c5c3d1cda377a4
randomplayer2002 <76877728+randomplayer2002@users.noreply.github.com>
2022-08-23 11:04:07 +0530

    Done

--- WarshallAlgo.java
+++ WarshallAlgo.java
@@ -1,81 +1,44 @@
 package Daa;
 import java.util.Scanner;
-
-/** Class Warshall **/
-public class Warshall
+public class Warshal {
+static int a[][];
+static int n;
+public static void main(String args[])
 {
-private int V;
-private boolean[][] tc;
-  
-/** Function to make the transitiveclosure **/
-
-public void getTC(int[][] graph)
+System.out.println("Enter the number of vertices\n");
+Scanner scanner = new Scanner(System.in);
+n = scanner.nextInt();
+a = new int[n][n];
+System.out.println("Enter the Cost Matrix (0's and 1's) \n");
+for (int i = 0; i < n; i++)
 {
-this.V = graph.length;
-tc = new boolean[V][V];
-for (int i = 0; i < V; i++)
+for (int j = 0; j < n; j++)
 {
-for (int j = 0; j< V; j++)
-if (graph[i][j] != 0)
-tc[i][j] = true;
-// tc[i][i] = true;
+a[i][j] = scanner.nextInt();
 }
-for (int i = 0; i < V; i++)
-{
-for (int j = 0; j < V; j++)
-{
-if (tc[j][i])
-for (int k = 0; k < V;k++)
-if (tc[j][i] && tc[i][k])
-tc[j][k] = true;
-    }
-  }
-
 }
-/** Funtion to display the trasitiveclosure **/
-  
-public void displayTC()
+getClosure();
+PrintMatrix();
+scanner.close();
+}
+public static void getClosure()
 {
-System.out.println("\nTransitive closure :\n");
-//System.out.print(&quot; &quot;);
-//for (int v = 0; v &lt; V; v++)
-//System.out.print(&quot; &quot; + v );
+for (int k = 0; k < n; k++)
+{
+for (int i = 0; i < n; i++)
+for (int j = 0; j < n; j++)
+if(a[i][j]==1 || (a[i][k]==1 && a[k][j]==1))
+a[i][j]=1;
+}
+}
+public static void PrintMatrix()
+{
+System.out.println("Transitive Closure:\n");
+for(int i=0; i<n; i++)
+{
+for(int j=0; j<n; j++)
+System.out.print(a[i][j] +" ");
 System.out.println();
-  
-for (int v = 0; v < V; v++)
-{
-//System.out.print(v +" ");
-for (int w = 0; w < V; w++)
-{
-  if (tc[v][w])
-    System.out.print(" 1 ");
-  else
-    System.out.print(" 0 ");
 }
-  System.out.println();
-  }
 }
-/** Main function **/
-public static void main (String[] args)
-{
-Scanner scan = new Scanner(System.in);
-System.out.println("WarshallAlgorithm Test\n");
-
-/** Make an object of Warshall class**/
-Warshall w = new Warshall();
-  
-/** Accept number of vertices **/
-System.out.println("Enter number ofvertices\n");
-int V = scan.nextInt();
-  
-/** get graph **/
-System.out.println("\nEntermatrix\n");
-  
-int[][] graph = new int[V][V];
-for (int i = 0; i < V; i++)
-for (int j = 0; j < V; j++)
-graph[i][j] = scan.nextInt();
-w.getTC(graph);
-w.displayTC();
-  }
 }
